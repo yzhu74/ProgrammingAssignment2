9126b5ab4b02396cfbfc30c0eaa28a0dada6f0f6
yzhu74 <yizhangzhu0608@gmail.com>
2020-04-12 00:12:54 -0400

    makeCacheMatrix.r

---
+++ makeCacheMatrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solveMatrix) inverse <<- solveMatrix
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
