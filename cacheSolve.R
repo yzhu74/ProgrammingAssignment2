d092f84d1f54c74057b9b7b3d8df9f3ac7d985c6
yzhu74 <yizhangzhu0608@gmail.com>
2020-04-12 00:26:59 -0400

    cacheSolve.R

---
+++ cacheSolve.R
@@ -0,0 +1,11 @@
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
+} 
